d8a40f452e4ec6bc74f6ea3472bdcc3849dc47b9
Chris May <chmay@redhat.com>
2024-03-15 13:35:51 -0400

    Add a second method to ShippingService
    Added a second method to ShippingService (and exposed the method for
    invocation via remote EJB by adding to ShippingServiceRemote).

--- src/main/java/com/redhat/coolstore/service/ShippingService.java
+++ src/main/java/com/redhat/coolstore/service/ShippingService.java
@@ -1,5 +1,8 @@
 package com.redhat.coolstore.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
@@ -9,6 +12,7 @@ import com.redhat.coolstore.model.ShoppingCart;
 @Remote
 public class ShippingService implements ShippingServiceRemote {
 
+    @Override
     public double calculateShipping(ShoppingCart sc) {
 
         if (sc != null) {
@@ -39,6 +43,36 @@ public class ShippingService implements ShippingServiceRemote {
 
         return 0;
 
+    }
+
+    @Override
+    public double calculateShippingInsurance(ShoppingCart sc) {
+
+        if (sc != null) {
+
+            if (sc.getCartItemTotal() >= 25 && sc.getCartItemTotal() < 100) {
+
+                return getPercentOfTotal(sc.getCartItemTotal(), 0.02);
+
+            } else if (sc.getCartItemTotal() >= 100 && sc.getCartItemTotal() < 500) {
+
+                return getPercentOfTotal(sc.getCartItemTotal(), 0.015);
+
+            } else if (sc.getCartItemTotal() >= 500 && sc.getCartItemTotal() < 10000) {
+
+                return getPercentOfTotal(sc.getCartItemTotal(), 0.01);
+
+            }
+
+        }
+
+        return 0;
+    }
+
+    private static double getPercentOfTotal(double value, double percentOfTotal) {
+        return BigDecimal.valueOf(value * percentOfTotal)
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
     }
 
 }
--- src/main/java/com/redhat/coolstore/service/ShippingServiceRemote.java
+++ src/main/java/com/redhat/coolstore/service/ShippingServiceRemote.java
@@ -4,4 +4,5 @@ import com.redhat.coolstore.model.ShoppingCart;
 
 public interface ShippingServiceRemote {
     double calculateShipping(ShoppingCart sc);
+    double calculateShippingInsurance(ShoppingCart sc);
 }
